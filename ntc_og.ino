c8cb55e40d476c7b2800b7eb0108a2f4233e9c31
bethmcavoy7 <45666501+bethmcavoy7@users.noreply.github.com>
2022-04-22 12:33:06 -0400

    Add files via upload

---
+++ ntc_og.ino
@@ -0,0 +1,96 @@
+//int ThermistorPin = 0;
+//int Vo;
+//float R1 = 10000;
+//float logR2, R2, T, Tc, Tf;
+//float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+//
+//void setup() {
+//Serial.begin(9600);
+//}
+//
+//void loop() {
+//
+//  Vo = analogRead(ThermistorPin);
+//  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+//  logR2 = log(R2);
+//  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+//  Tc = T - 273.15;
+//  Tf = (Tc * 9.0)/ 5.0 + 32.0; 
+//
+//  Serial.print("Temperature: "); 
+//  Serial.print(Tf);
+//  Serial.print(" F; ");
+//  Serial.print(Tc);
+//  Serial.println(" C");   
+//
+//  delay(500);
+//}
+
+
+
+// 74HC4067 multiplexer demonstration (16 to 1)
+
+// control pins output table in array form
+// see truth table on page 2 of TI 74HC4067 data sheet
+// connect 74HC4067 S0~S3 to Arduino D7~D4 respectively
+// connect 74HC4067 pin 1 to Arduino A0
+byte controlPins[] = {B00000000, 
+                  B10000000,
+                  B01000000,
+                  B11000000,
+                  B00100000,
+                  B10100000,
+                  B01100000,
+                  B11100000,
+                  B00010000,
+                  B10010000,
+                  B01010000,
+                  B11010000,
+                  B00110000,
+                  B10110000,
+                  B01110000,
+                  B11110000 }; 
+
+// holds incoming values from 74HC4067                  
+byte muxValues[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+
+void setup()
+{
+  Serial.begin(9600);
+  DDRD = B11111111; // set PORTD (digital 7~0) to outputs
+}
+
+void setPin(int outputPin)
+// function to select pin on 74HC4067
+{
+  PORTD = controlPins[outputPin];
+}
+
+void displayData()
+// dumps captured data from array to serial monitor
+{
+  Serial.println();
+  Serial.println("Values from multiplexer:");
+  Serial.println("========================");
+  for (int i = 0; i < 16; i++)
+  {
+    Serial.print("input I"); 
+    Serial.print(i); 
+    Serial.print(" = "); 
+    Serial.println(muxValues[i]);
+  }
+  Serial.println("========================");  
+}
+
+void loop()
+{
+  for (int i = 0; i < 16; i++)
+  {
+    setPin(i); // choose an input pin on the 74HC4067
+    muxValues[i]=analogRead(0); // read the vlaue on that pin and store in array
+  }
+
+  // display captured data
+  displayData();
+  delay(2000); 
+}
---
+++ ntc_test.ino
@@ -0,0 +1,65 @@
+
+
+#define number_of_mux 1
+#define numOfMuxPins number_of_mux * 8
+#define enPin 2
+#define S0 3
+#define S1 4
+#define S2 5
+#define S3 6
+
+const int comPin  = 7;
+String incomingByte;   // for incoming serial data
+
+int Vo;
+float R1 = 10000;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT); 
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  
+  digitalWrite(S0, LOW);
+  digitalWrite(S1, LOW);
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  digitalWrite(enPin, LOW);
+
+}
+
+void loop() {
+  Serial.print("\ntype what channel to read\n");
+  while (Serial.available() == 0)   {
+  }
+    incomingByte = Serial.readString();
+    int in=incomingByte.toInt();
+    Serial.print("you said --\n");
+    Serial.print(in,BIN);
+    Serial.print("\n");
+//    Serial.print(bitRead(in,3));
+//    Serial.print(bitRead(in,2));
+//    Serial.print(bitRead(in,1));
+//    Serial.print(bitRead(in,0));
+
+    digitalWrite(S0, bitRead(in,3));
+    digitalWrite(S1, bitRead(in,2));
+    digitalWrite(S2, bitRead(in,1));
+    digitalWrite(S3, bitRead(in,0));
+
+    
+      Vo = analogRead(comPin);
+      Serial.print(Vo);
+      
+      R2 = R1 * (1023.0 / (float)Vo - 1.0);
+      logR2 = log(R2);
+      T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+      T = T - 273.15;
+      Serial.print("Temperature1: "); 
+      Serial.print(T);
+      Serial.println(" C");
+}
